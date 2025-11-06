a0f71a2616165b029a814f3b390becbee9c8d02f
3644272322 <adrianferna29@gmail.com>
2025-11-06 21:35:53 +0000

    add

--- python/variables/calculadora/cal-data.py
+++ python/variables/calculadora/cal-data.py
@@ -1,5 +1,28 @@
-num = 1
-num = 3
-op = "+"
-result = eval(f"{num1}{op}{num2}")
-print(result)
+n1 = input("numero 1: ")
+n2 = input("numero 2: ")
+op = input("operador: ")
+
+def hiterar_conjunto(n1 , n2):
+    for  n in list(n1) :
+        if n == "," or n == " " or n == "]" or n == "[":
+            continue
+        eval(f"{n} + {op} + {n}")
+        print(int (n) - int (n2))
+
+if op == "-":
+    if n1[0] =="[" and  n1[-1] == "]":
+        
+        print("\n",int(n1[1]) - int (n2))
+        print(int(n1[3]) -  int (n2), "\n")
+        hiterar_conjunto(n1, n2)
+        
+    # print (int(n1) - int(n2))  
+elif op =="+" :
+    print (int(n1) + int(n2))
+elif op  == "*":
+    print (int(n1)) * int(n2)
+elif op == "/":
+    print (int(n1) / int(n2))
+else:
+    print("error")
+
